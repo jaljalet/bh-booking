a5a5d82d2f49fa4fb165a12a1f4a5fb348f837ac
jaljalet <ilyasogluislam@gmail.com>
2025-02-03 13:00:38 +0300

    start booking

---
+++ bh-booking.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Bilisimhizmet Booking
+ * Plugin URI: https://github.com/jaljalet/bilisimhizmet-booking
+ * Description: Special plugin for hotel reservations, programs, therapy for TheLifeCo https://thelifeco.com/
+ * Version: 1.0.0
+ * Author: Islam Ilyasoglu
+ * Author URI: https://bilisimhizmet.com/
+ * Text Domain: bh-booking
+ */
+
+ if( ! defined( 'ABSPATH' ) ) exit();
